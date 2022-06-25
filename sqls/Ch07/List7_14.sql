7a9132a1fc4607877c346753150347d534610c9d
しーたる <62013801+thetaru@users.noreply.github.com>
2022-06-26 01:50:46 +0900

    Update List7_14.sql

--- sqls/Ch07/List7_14.sql
+++ sqls/Ch07/List7_14.sql
@@ -9,8 +9,8 @@ from
     TenpoShohin as TS
     inner join Shohin as S
         on TS.shohin_id = S.shohin_id
-            inner join ZaikoShohin as ZS
-                on TS.shohin_id = ZS.shohin_id
+    inner join ZaikoShohin as ZS
+        on TS.shohin_id = ZS.shohin_id
 where
     ZS.souko_id = 'S001'
 ;
